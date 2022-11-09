0a188f90ef53ec9c6e756d1baa6668e7b86b8a39
PranxXxD <saipranayalishetty@gmail.com>
2022-11-09 20:04:09 +0530

    Time: 92 ms (60.01%), Space: 42.1 MB (70.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,22 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for(let i=0 ; i< nums.length; i++){
-        if(nums[i] >= target){
-            return i
-        }
+    // for(let i=0 ; i< nums.length; i++){
+    //     if(nums[i] >= target){
+    //         return i
+    //     }
+    // }
+    // return nums.length
+    let first = 0
+    let last = nums.length-1
+    
+    while(first <= last){
+        let mid = Math.floor(first + (last - first) / 2);
+        if(nums[mid] > target){
+            last = mid-1
+        }else if(nums[mid] < target){
+            first = mid+1
+        }else return mid
     }
-    return nums.length
+    return first
 };
