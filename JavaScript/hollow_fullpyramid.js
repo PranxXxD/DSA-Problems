f08b2cc70d784786eb90e6abbe915b554c7dcb69
PranxXxD <saipranayalishetty@gmail.com>
2024-01-03 20:44:01 +0530

    Update hollow_fullpyramid.js

--- JavaScript/hollow_fullpyramid.js
+++ JavaScript/hollow_fullpyramid.js
@@ -26,3 +26,18 @@ function pyramid(){
   }
   
   pyramid()
+
+
+function NormalPyramid(){
+  let value = prompt("Enter a value:")
+  
+  for (var row = 0; row < value; row++) {
+    let str;
+    for (var col = 0; col < row+1; col++) {
+              str += "*"
+    }
+    console.log(str)
+  }
+}
+
+NormalPyramid()
